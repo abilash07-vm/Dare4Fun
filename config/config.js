cec4993a3491c8cc69c933b13d52c132ceb089b8
Abilash VM <abil17ec004@rmkcet.ac.in>
2021-06-21 12:31:14 +0530

    Delete config.js

--- config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    "MONGO_DB_URL":"mongodb+srv://abilashvm07:Biceps^17@cluster-1.stf1z.mongodb.net/Dare4Fun?retryWrites=true&w=majority",
-    "MAIL_ID":"dare4fun123@gmail.com",
-    "PASSWORD":"Biceps^17"
-}
